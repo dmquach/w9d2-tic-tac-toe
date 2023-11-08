2ade9d2d6553c35119e209dda6ec7a64d4e8b082
Dylan <dylanquach@gmail.com>
2023-11-07 19:42:37 -0800

    Add css

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -2,6 +2,7 @@ import Game from "../ttt_node/game";
 import View from "./ttt-view";
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
-  const view = document.getElementsByClassName("ttt")
-  new Game()
+  const view = document.querySelector(".ttt")
+  const game = new Game()
+  new View(game, view)
 });
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,19 +1,25 @@
 class View {
   constructor(game, el) {
+    this.game = game
+    this.el = el
+    this.setupBoard()
   }
 
   setupBoard() {
-    let grid = document.createElement('ul')
+    let grid = document.createElement("ul")
     for(let i = 0; i < 3 ; i++){
       for(let j = 0; j < 3; j++){
-        let point = document.createElement('li')
+        let point = document.createElement("li");
         point.dataset.pos = [i,j]
         grid.appendChild(point)
       }
-    }    
+    }
+    this.el.appendChild(grid);
+    this.el.addEventListener("click", this.handleClick)
   }
 
   handleClick(e) {
+    const el = e.target
   }
 
   makeMove(square) {
